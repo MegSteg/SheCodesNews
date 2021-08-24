7d2fd88de1e350b9f98747e43a073c9bb7323475
MegSteg <86500966+MegSteg@users.noreply.github.com>
2021-08-24 20:34:02 +1000

    working so far!

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
@@ -8,8 +8,8 @@ class NewsStory(models.Model):
         on_delete=models.CASCADE
     )
     pub_date = models.DateTimeField()
+    image_url = models.URLField(max_length=400, default= "https://picsum.photos/600")
     content = models.TextField()
-    image_url = models.TextField()
 
     class Meta:
         ordering = ['-pub_date']
