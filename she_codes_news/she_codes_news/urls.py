faeffd8ef2c11c78ebddffe9406468c2c61a199e
MegSteg <86500966+MegSteg@users.noreply.github.com>
2021-09-04 14:38:13 +1000

    Update urls.py

--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    path('index', include (url='news/'))
+    path ('index/', include ('news.urls')),
+
 ]
+
